8b9cb80634f9711f5460d93f03ac627bcad13abd
Devin Ercolano <devin.ercolano@gmail.com>
2021-06-25 10:49:50 -0700

    trying to display Firebase collection information on a view.

--- SeshatOrchidProject/Controllers/ViewController.swift
+++ SeshatOrchidProject/Controllers/ViewController.swift
@@ -7,13 +7,94 @@
 
 import UIKit
 import SwiftUI
+import Firebase
 
 class ViewController: UITabBarController, UITabBarControllerDelegate {
+    @Published var results = [Poem]()
+
+    var db: Firestore!
+    
+    func getDocument() {
+        Firestore.firestore().collection("poems").getDocuments { (snapshot, err) in
+                  if let err = err {
+                      print("Error getting documents: \(err)")
+                  } else {
+                      for document in snapshot!.documents {
+//                         let docId = document.documentID
+                        let poemsNSDictionary = document.data() as NSDictionary
+                        //Testing: did Firebase supply the poems document
+//                        print("poemsNSDictionary", poemsNSDictionary)
+                        
+                        // get JSON-ed poems
+//                        if let jsonPoemData = try? JSONSerialization.data(withJSONObject: poemsNSDictionary, options: .prettyPrinted) {
+//                        print("jsonPoemData ", jsonPoemData)
+                            
+
+                        let nsDictionaryData = try? JSONSerialization.data(
+                            withJSONObject: poemsNSDictionary,
+                            options: []
+                        )
+                        
+//                        if let jsonPoemString = String(data: jsonPoemData, encoding: .utf8) {
+//                        print("stringOfJSON", jsonPoemString)
+//
+
+                        // Swift Dictionary To Data.
+                        do {
+                            // need json for from:
+                            let res = try JSONDecoder().decode(Poems.self, from: nsDictionaryData!)
+                            self.results.append(contentsOf: res.poems)
+                            print("results ", self.results)
+                            print("results count: \(self.results.count)")
+                        } catch{
+                            print("My error message", error)
+                        }
+                        
+//                        DispatchQueue.main.async {
+//                            do {
+//
+//                            } catch {
+//                                print("Error: \(error)")
+//                                #if DEBUG
+//                                fatalError()
+//                                #endif
+//                            }
+                        //convert from NSDictionary to String
+                        
+//                        if let jsonPoemData = try? JSONSerialization.data(withJSONObject: poemsData, options: []) {
+//                            if let jsonPoemString = String(data: jsonPoemData, encoding: .utf8) {
+//                                print("dictFromJSON", jsonPoemString)
+//
+//                                // store poems data as [PoemModel]
+//                                var poemModels: [PoemModel] = []
+//                                for poem in jsonPoemString {
+//                                    //put it into a model
+//                                    let addPoem = PoemModel()
+//                                    addPoem.id = 9999999
+//                                    addPoem.title = poem["title"]
+//                                    addPoem.poemText = poem["poemText"]
+//                                    addPoem.tags = poem["tags"]
+//
+//                                    poemModels.append(addPoem)
+//                                }
+//                                 print(poemsData)
+                            //}
+                      // }
+
+                        //}
+                        
+                        
+                      }
+                  }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Assign self for delegate for that ViewController can respond to UITabBarControllerDelegate methods
         self.delegate = self
+        getDocument()
     }
     
     override func viewWillAppear(_ animated: Bool) {
