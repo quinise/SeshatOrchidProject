38fda1bf229c00a6411ad282c53e27ccd906d234
Devin Ercolano <devin.ercolano@gmail.com>
2021-07-06 09:04:20 -0700

    adding a new table to the database, cosmetic changes to PoemView.

---
+++ SeshatOrchidProject/Poem.swift
@@ -0,0 +1,12 @@
+//  SeshatOrchidProject
+//
+//  Created by Devin Ercolano on 6/19/21.
+//
+import Foundation
+
+class Poem: Identifiable {
+    public var id: Int64 = 0
+    public var title: String = ""
+    public var poemText: String = ""
+    public var tags: String = ""
+}
---
+++ SeshatOrchidProject/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  SeshatOrchidProject
+//
+//  Created by Devin Ercolano on 7/6/21.
+//
+
+import Foundation
+
+class Story: Identifiable {
+    public var storyId: Int64 = 0
+    public var storyTitle: String = ""
+    public var storyText: String = ""
+//    public var picture: String = ""
+    public var storyTags: String = ""
+}
