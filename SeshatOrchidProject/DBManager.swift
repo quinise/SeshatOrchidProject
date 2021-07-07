b7667a305dbec24e8cd2777d4166a5100e223353
Devin Ercolano <devin.ercolano@gmail.com>
2021-07-07 04:36:57 -0700

    updated poems tab to include pictures, themes, and titles (modularized that code into a new file). Updated PoemsView to include a main image.

--- SeshatOrchidProject/DBManager.swift
+++ SeshatOrchidProject/DBManager.swift
@@ -17,6 +17,7 @@ class DBManager {
     private var id: Expression<Int64>!
     private var title: Expression<String>!
     private var poemText: Expression<String>!
+    private var poemPicture: Expression<String>!
     private var tags: Expression<String>!
     
     private var storyId: Expression<Int64>!
@@ -34,6 +35,7 @@ class DBManager {
             id = Expression<Int64>("id")
             title = Expression<String>("title")
             poemText = Expression<String>("poemText")
+            poemPicture = Expression<String>("poemPicture")
             tags = Expression<String>("tags")
 
             if (!UserDefaults.standard.bool(forKey: "is_db_created")) {
@@ -41,28 +43,29 @@ class DBManager {
                     t.column(id, primaryKey: true)
                     t.column(title)
                     t.column(poemText, unique: true)
+                    t.column(poemPicture)
                     t.column(tags)
                 })
 
                 UserDefaults.standard.bool(forKey: "is_db_created")
              }
             
-            stories = Table("stories")
-            
-            storyId = Expression<Int64>("storyId")
-            storyTitle = Expression<String>("storyTitle")
-            storyText = Expression<String>("storyText")
-            storyTags = Expression<String>("storyTags")
-            
-            if (!UserDefaults.standard.bool(forKey: "is_db_created")) {
-                try db.run(stories.create { (t) in
-                    t.column(storyId, primaryKey: true)
-                    t.column(storyTitle)
-                    t.column(storyText, unique: true)
-                    t.column(storyTags)
-                })
-            }
-            
+//            stories = Table("stories")
+//            
+//            storyId = Expression<Int64>("storyId")
+//            storyTitle = Expression<String>("storyTitle")
+//            storyText = Expression<String>("storyText")
+//            storyTags = Expression<String>("storyTags")
+//            
+//            if (!UserDefaults.standard.bool(forKey: "is_db_created")) {
+//                try db.run(stories.create { (t) in
+//                    t.column(storyId, primaryKey: true)
+//                    t.column(storyTitle)
+//                    t.column(storyText, unique: true)
+//                    t.column(storyTags)
+//                })
+//            }
+//            
         } catch {
             print("create poems table", error.localizedDescription)
         }
@@ -95,8 +98,10 @@ class DBManager {
     public func getStories() -> [Story] {
         var storyModels: [Story] = []
         print("just made story models array in getStories")
+        
         stories = stories.order(storyId.desc)
         print("just made table be in decending order")
+        
         do {
             for story in try db.prepare(stories) {
                 print("in for loop")
--- SeshatOrchidProject/Poem.swift
+++ SeshatOrchidProject/Poem.swift
@@ -7,6 +7,15 @@ import Foundation
 class Poem: Identifiable {
     public var id: Int64 = 0
     public var title: String = ""
+    public var poemPicture: String = ""
     public var poemText: String = ""
     public var tags: String = ""
+    
+    var mainImage: String {
+        title.replacingOccurrences(of: " ", with: "-").lowercased()
+    }
+
+    var thumbnailImage: String {
+        "\(mainImage)-thumb"
+    }
 }
